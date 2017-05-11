0b007f23f539be0af856629ae0fc2a57655f9268
Bruce A. Lee <brucelee774@hotmail.com>
2017-05-10 22:13:10 -0500

    Add association between Score and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511025122) do
+ActiveRecord::Schema.define(version: 20170511031153) do
 
   create_table "scores", force: :cascade do |t|
     t.integer  "game"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170511025122) do
     t.integer  "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_scores_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
